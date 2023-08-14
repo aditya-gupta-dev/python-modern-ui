37da7b27a34909fbfc46bf3cebe6534a74be2abc
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-14 09:58:42 +0530

    Update main.py
    added main function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, World !")
+from listPanel import ListPanel
+from window import Window
+from inputPanel import InputPanel
+
+def main():
+    window = Window()
+
+    listPanel = ListPanel(window)
+    inputPanel = InputPanel(window, listPanel)
+    
+    inputPanel.add()
+    listPanel.add()
+    window.show()
+main()
